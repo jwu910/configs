78e6b27fdad042d21ecabc1df2b9724353de0d9f
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-12-22 01:38:26 -0800

    Add startup script. SF polybar script

--- scripts/polybar/polybar.sh
+++ scripts/polybar/polybar.sh
@@ -8,5 +8,3 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar top -c ~/configs/statusbar/polybar/polybar.conf &
 polybar bottom -c ~/configs/statusbar/polybar/polybar.conf &
-
-
